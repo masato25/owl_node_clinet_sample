f77f3af8855d89a2c541757847209895d502b2be
masato25 <leomasato25@gmail.com>
2016-02-23 15:09:56 +0800

    update proto files and api naming

--- example/client.js
+++ example/client.js
@@ -1,11 +1,11 @@
-var PROTO_PATH = __dirname + '/grafana.proto';
+var PROTO_PATH = __dirname + '/owlapi.proto';
 
 var grpc = require('../');
-var grafana_proto = grpc.load(PROTO_PATH).grafana;
+var grafana_proto = grpc.load(PROTO_PATH).owlapi;
 
 
 function main() {
-  var client = new grafana_proto.OwlQuery('localhost:8080',
+  var client = new grafana_proto.OwlQuery('10.0.0.167:8080',
                                        grpc.credentials.createInsecure());
   var querystr;
   if (process.argv.length >= 3) {
@@ -15,11 +15,12 @@ function main() {
   }
   //queryrrd(1452806153, 1452827753, "AVERAGE", "docker-agent", "cpu.idle")
   var start_ts = 1452806153;
-  var end_ts = 1452827753;
+  var end_ts = 1456108228;
   var consolfun = "AVERAGE";
   var endpoint = "docker-agent";
   var counter = "cpu.idle";
-  client.query({start_ts: start_ts, end_ts: end_ts, consolfun: consolfun, endpoint: endpoint, counter: counter}, function(err, response) {
+
+  client.query({startTs: start_ts, endTs: end_ts, computeMethod: consolfun, endpoint: endpoint, counter: counter}, function(err, response) {
     var result = JSON.parse(response.result);
     console.log(result);
   });
